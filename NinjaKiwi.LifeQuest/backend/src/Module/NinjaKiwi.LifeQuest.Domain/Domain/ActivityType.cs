be1f678c4f30bb937ab9ecfb3077111a4970e55a
Mishalia Pillay <Mishalia.Pillay@boxfusion.io>
2025-05-20 21:02:42 +0200

    activity types added but no app service

---
+++ NinjaKiwi.LifeQuest/backend/src/Module/NinjaKiwi.LifeQuest.Domain/Domain/ActivityType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities.Auditing;
+using Shesha.Domain.Attributes;
+
+namespace NinjaKiwi.LifeQuest.Domain.Domain
+{
+    [Entity(TypeShortAlias = "LifQu.ActivityType")]
+    public class ActivityType : FullAuditedEntity<Guid>
+    {  /// <summary>
+       /// The category of the exercise
+       /// </summary>
+        public virtual string Category { get; set; }
+        /// <summary>
+        /// The calories of the exercise
+        /// </summary>
+        public virtual int Calories { get; set; }
+        /// <summary>
+        /// The description of exercise
+        /// </summary>
+        public virtual string Description { get; set; }
+        /// <summary>
+        /// The duration of the exercise
+        /// </summary>
+        public virtual string Duration { get; set; }
+
+        // Navigation property to related activities
+        //  public virtual IList<ActivityActivityType> Activities { get; set; } = new List<ActivityActivityType>();
+    }
+}
---
+++ NinjaKiwi.LifeQuest/backend/src/Module/NinjaKiwi.LifeQuest.Domain/Migrations/M20250520205011.cs
@@ -0,0 +1,25 @@
+﻿
+using FluentMigrator;
+using Shesha.FluentMigrator;
+namespace NinjaKiwi.LifeQuest.Domain.Migrations
+{
+    [Migration(20250520090100)]
+    public class M20250520090100_CreateActivityType : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("LifQu_ActivityTypes")
+                .WithIdAsGuid()
+                .WithFullAuditColumns()
+                .WithColumn("Category").AsString(255).Nullable()
+                .WithColumn("Calories").AsInt32().Nullable()
+                .WithColumn("Description").AsString(1000).Nullable()
+                .WithColumn("Duration").AsString(100).Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("LifeQu_ActivityTypes");
+        }
+    }
+}
